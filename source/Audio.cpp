a45295ad093b3bfdaeb9ad61aac4db19f679e278
Rüzgar Koray Genç <koray_genc1@hotmail.com>
2026-01-19 20:28:18 +0300

    Midi port selection

--- source/Audio.cpp
+++ source/Audio.cpp
@@ -11,18 +11,36 @@ bool Audio::Init()
 
     try
     {
-        // Auto-connect to the first available device
         unsigned int portCount = midi.GetRtMidi()->getPortCount();
+
+        // 1. List all devices to the console so you can see them
+        std::cout << "Available MIDI Ports:\n";
+        int bestPort = -1;
+
+        for (unsigned int i = 0; i < portCount; i++) {
+            std::string name = midi.GetRtMidi()->getPortName(i);
+            std::cout << "  [" << i << "] " << name << "\n";
+
+            if (name.find("MIDI"))
+            {
+                bestPort = i;
+            }
+        }
+
+        // 2. Open the best guess (or default to 0)
         if (portCount > 0)
         {
-            midi.GetRtMidi()->openPort(0); // Open first device
+            int targetPort = (bestPort != -1) ? bestPort : 0;
+
+            midi.GetRtMidi()->openPort(bestPort);
             midi.GetRtMidi()->setCallback(&MIDI::midiCallback, nullptr);
-            midi.GetRtMidi()->ignoreTypes(false, false, false); // Don't ignore SysEx/Timing
-            std::cout << "Connected to MIDI Device: " << midi.GetRtMidi()->getPortName(0) << "\n";
+            midi.GetRtMidi()->ignoreTypes(false, false, false);
+
+            std::cout << "Connected to MIDI Device: " << midi.GetRtMidi()->getPortName(targetPort) << "\n";
         }
         else
         {
-            std::cout << "No MIDI devices found. (You can still use the visualizer)\n";
+            std::cout << "No MIDI devices found.\n";
         }
 
         return true;
--- source/Renderer.h
+++ source/Renderer.h
@@ -7,8 +7,10 @@ public:
 	bool Init(SDL_Window* window);
 	void Clear();
 	void Present();
+
 	void SetDrawColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
 	void FillRectangle(const SDL_FRect* rect);
+
 	SDL_Renderer* GetSDLRenderer();
 	void ShutDown();
 private:
